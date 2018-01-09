f10137b06e5cf6b9ac1b4c1adebe1a5f84f0d998
Martijn Stegeman <martijn@stgm.nl>
2018-01-09 17:57:02 +0100

    piramide test 1 en 2 met behulp van RE
    nu kloppen ze ook echt

--- tests/module1/piramideTest.py
+++ tests/module1/piramideTest.py
@@ -5,8 +5,9 @@ import re
 
 @t.test(0)
 def exactMario0(test):
-	test.test = lambda : not assertlib.contains(lib.outputOf(_fileName, stdinArgs=[1]), "##")
-	test.description = lambda : "print een pyramide van 0 hoog"
+  test.test = lambda : assertlib.match(lib.outputOf(_fileName, stdinArgs=[1]),
+    re.compile(".*(# #)[ ]*(\n)"))
+  test.description = lambda : "print een welgevormde pyramide van 1 hoog"
 
 @t.test(1)
 def exactMario3(test):
@@ -16,11 +17,11 @@ def exactMario3(test):
       "(  # # #)[ ]*(\n)"
       "(# # # #)[ ]*"
       ".*", re.MULTILINE))
-  test.description = lambda : "print een pyramide van 3 hoog"
+  test.description = lambda : "print een welgevormde pyramide van 3 hoog"
 
 @t.test(2)
 def exactMario23(test):
-	test.test = lambda : assertlib.match(lib.outputOf(_fileName, stdinArgs=[23]),
+  test.test = lambda : assertlib.match(lib.outputOf(_fileName, stdinArgs=[23]),
     re.compile(".*"
       "(                                            # #)[ ]*(\n)"
       "(                                          # # #)[ ]*(\n)"
@@ -46,10 +47,11 @@ def exactMario23(test):
       "(  # # # # # # # # # # # # # # # # # # # # # # #)[ ]*(\n)"
       "(# # # # # # # # # # # # # # # # # # # # # # # #)[ ]*"
       ".*", re.MULTILINE))
-	test.description = lambda : "print een pyramide van 23 hoog"
+  test.description = lambda : "print een welgevormde pyramide van 23 hoog"
 
 
 @t.test(10)
 def handlesWrongInput(test):
-	test.test = lambda : not assertlib.contains(lib.outputOf(_fileName, stdinArgs=[-100, 100, 24, 1]), "##")
-	test.description = lambda : "handelt een verkeerde input van -100, 100 en 24 af"
+  test.test = lambda : assertlib.match(lib.outputOf(_fileName, stdinArgs=[-100, 100, 24, 1]),
+    re.compile(".*(# #)[ ]*(\n)"))
+  test.description = lambda : "handelt verkeerde input netjes af"
