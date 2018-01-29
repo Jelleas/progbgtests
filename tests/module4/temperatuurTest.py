67207402dd3495d22f21fb6af28e2e0ac6219744
jelleas <jelle.van.assema@gmail.com>
2018-01-29 23:01:59 +0100

    refactored temperatuur

--- tests/module4/temperatuurTest.py
+++ tests/module4/temperatuurTest.py
@@ -2,7 +2,7 @@ import checkpy.tests as t
 import checkpy.lib as lib
 import checkpy.assertlib as assertlib
 import importlib
-
+import helpers
 import re
 
 def sandbox():
@@ -28,172 +28,132 @@ def after():
 	except ImportError:
 		pass
 
-
 # Thanks to Vera Schild!
 
 @t.test(10)
 def correctHighestTemp(test):
 	def testMethod():
+		correctAnswer = "36.8"
+		if helpers.isHardcodedIn(correctAnswer, test.fileName):
+			test.success = lambda info : "let op: deze output is hardcoded. {} staat in de source code!".format(correctAnswer)
+
 		output = lib.outputOf(
 			test.fileName,
 			overwriteAttributes = [("__name__", "__main__")]
 		)
-		return assertlib.contains(output, '36.8')
+		return assertlib.contains(output, correctAnswer)
+
 	test.test = testMethod
 	test.description = lambda : "print hoogste temperatuur"
 
 @t.passed(correctHighestTemp)
 @t.test(11)
 def correctDateHighestTemp(test):
-<<<<<<< HEAD
-	def findline(outputOf):
-		for line in outputOf.split("\n"):
-			if assertlib.contains(line, '36.8'):
-				return line
-		return 0
-
-	outputLine = findline(lib.outputOf(_fileName))
-
-	tsts = ['6', 'juni', 'June']
-	test.test = lambda : assertlib.contains(outputLine, '27') and\
-			     sum([assertlib.contains(outputLine, tst) for tst in tsts])\
-		             and assertlib.contains(outputLine, '1947')
-
-
-	output = lib.outputOf(_fileName).split("\n")
-	source = " ".join(lib.removeComments(lib.source(_fileName)).split("\n"))
-
-	if re.compile(".*" + outputLine + ".*").match(source):
-		test.description = lambda : "let op: deze output is hardcoded. Dit geldt voor deze en de bovenstaande test"
-				
-	else:
-		test.description = lambda : "print datum hoogste temperatuur"
-
-=======
 	def testMethod():
+		if helpers.isHardcodedIn(1947, test.fileName):
+			test.success = lambda info : "let op: deze output is hardcoded. 1947 staat in de source code!"
+
 		output = lib.outputOf(
 			test.fileName,
 			overwriteAttributes = [("__name__", "__main__")]
 		)
-		correctDay = assertlib.contains(output, '27')
-		correctMonth = any([assertlib.contains(output.lower(), month) for month in ["6", "juni", "june", "jun"]])
-		correctYear = assertlib.contains(output, '1947')
+		line = helpers.findLineWith(output, "36.8")
+
+		correctDay = assertlib.contains(line, '27')
+		correctMonth = any([assertlib.contains(line.lower(), month) for month in ["6", "juni", "june", "jun"]])
+		correctYear = assertlib.contains(line, '1947')
 		return correctDay and correctMonth and correctYear
 
 	test.test = testMethod
 	test.description = lambda : "print datum hoogste temperatuur"
->>>>>>> a5f64393a485fff527e9533710d9a6f49938c05d
 
 @t.test(20)
 def correctLowestTemp(test):
 	def testMethod():
+		correctAnswer = "-24.8"
+		if helpers.isHardcodedIn(correctAnswer, test.fileName):
+			test.success = lambda info : "let op: deze output is hardcoded. {} staat in de source code!".format(correctAnswer)
+
 		output = lib.outputOf(
 			test.fileName,
 			overwriteAttributes = [("__name__", "__main__")]
 		)
-		return assertlib.contains(output, '-24.8')
+		return assertlib.contains(output, correctAnswer)
+
 	test.test = testMethod
 	test.description = lambda : "print laagste temperatuur"
 
 @t.passed(correctLowestTemp)
 @t.test(21)
 def correctDateLowestTemp(test):
-<<<<<<< HEAD
-	def findline(outputOf):
-		for line in outputOf.split("\n"):
-			if assertlib.contains(line, '-24.8'):
-				return line
-		return 0
-
-	outputLine = findline(lib.outputOf(_fileName))
-
-	tsts = ['1', 'januari', 'January']
-	test.test = lambda : assertlib.contains(outputLine, '27') and\
-			     sum([assertlib.contains(outputLine, tst) for tst in tsts])\
-		             and assertlib.contains(outputLine, '1942')
-
-	output = lib.outputOf(_fileName).split("\n")
-	source = " ".join(lib.removeComments(lib.source(_fileName)).split("\n"))
-
-	if re.compile(".*" + outputLine + ".*").match(source):
-		test.description = lambda : "Let op: deze output is hardcoded. Dit geldt voor deze en de bovenstaande test"
-
-	else:
-		test.description = lambda : "print datum laagste temperatuur"
-=======
 	def testMethod():
+		if helpers.isHardcodedIn(1942, test.fileName):
+			test.success = lambda info : "let op: deze output is hardcoded. 1942 staat in de source code!"
+
 		output = lib.outputOf(
 			test.fileName,
 			overwriteAttributes = [("__name__", "__main__")]
 		)
-		correctDay = assertlib.contains(output, '27')
-		correctMonth = any([assertlib.contains(output.lower(), month) for month in ["1", "januari", "january", "jan"]])
-		correctYear = assertlib.contains(output, '1942')
+		line = helpers.findLineWith(output, "-24.8")
+
+		correctDay = assertlib.contains(line, '27')
+		correctMonth = any([assertlib.contains(line.lower(), month) for month in ["1", "januari", "january", "jan"]])
+		correctYear = assertlib.contains(line, '1942')
 		return correctDay and correctMonth and correctYear
 
 	test.test = testMethod
 	test.description = lambda : "print datum laagste temperatuur"
->>>>>>> a5f64393a485fff527e9533710d9a6f49938c05d
-
 
 @t.test(30)
 def correctLongestFreezing(test):
 	def testMethod():
+		correctAnswer = "21"
+		if helpers.isHardcodedIn(correctAnswer, test.fileName):
+			test.success = lambda info : "let op: deze output is hardcoded. {} staat in de source code!".format(correctAnswer)
+
 		output = lib.outputOf(
 			test.fileName,
 			overwriteAttributes = [("__name__", "__main__")]
 		)
-		return assertlib.contains(output, '21')
+		return assertlib.contains(output, correctAnswer)
+
 	test.test = testMethod
 	test.description = lambda : "print de langste periode dat het aaneengesloten heeft gevroren"
 
 @t.passed(correctLongestFreezing)
 @t.test(31)
 def correctDateLongestFreezingp(test):
-<<<<<<< HEAD
-	def findline(outputOf):
-		for line in outputOf.split("\n"):
-			if assertlib.contains(line, '21'):
-				return line
-		return 0
-
-	outputLine = findline(lib.outputOf(_fileName))	
-
-	tsts = ['2', 'februari', 'February']
-	test.test = lambda : assertlib.contains(outputLine, '24') and\
-			     sum([assertlib.contains(outputLine, tst) for tst in tsts])\
-		             and assertlib.contains(outputLine, '1947')
-
-	output = lib.outputOf(_fileName).split("\n")
-	source = " ".join(lib.removeComments(lib.source(_fileName)).split("\n"))
-
-	if re.compile(".*" + outputLine + ".*").match(source):
-		test.description = lambda : "let op: deze output is hardcoded. Dit geldt voor deze en de bovenstaande test"
-	else:
-		test.description = lambda : "print laatste dag van de langste periode dat het aaneengesloten heeft gevroren"
-=======
 	def testMethod():
+		if helpers.isHardcodedIn(1947, test.fileName):
+			test.success = lambda info : "let op: deze output is hardcoded. 1947 staat in de source code!"
+
 		output = lib.outputOf(
 			test.fileName,
 			overwriteAttributes = [("__name__", "__main__")]
 		)
-		correctDay = assertlib.contains(output, '24')
-		correctMonth = any([assertlib.contains(output.lower(), month) for month in ["2", "februari", "february", "feb"]])
-		correctYear = assertlib.contains(output, '1947')
+		line = helpers.findLineWith(output, "21")
+
+		correctDay = assertlib.contains(line, '24')
+		correctMonth = any([assertlib.contains(line.lower(), month) for month in ["2", "februari", "february", "feb"]])
+		correctYear = assertlib.contains(line, '1947')
 		return correctDay and correctMonth and correctYear
 
 	test.test = testMethod
 	test.description = lambda : "print laatste dag van de langste periode dat het aaneengesloten heeft gevroren"
->>>>>>> a5f64393a485fff527e9533710d9a6f49938c05d
 
 @t.test(40)
 def correctFirstHeatWave(test):
 	def testMethod():
+		correctAnswer = "1911"
+		if helpers.isHardcodedIn(correctAnswer, test.fileName):
+			test.success = lambda info : "let op: deze output is hardcoded. {} staat in de source code!".format(correctAnswer)
+
 		output = lib.outputOf(
 			test.fileName,
 			overwriteAttributes = [("__name__", "__main__")]
 		)
-		return assertlib.contains(output, '1911')
+		return assertlib.contains(output, correctAnswer)
+
 	test.test = testMethod
 	test.description = lambda : "print het eerste jaartal waarin er sprake was van een hittegolf"
 
